695e29117a4320c9cf631bdf262160aa79d577da
Expant <anton532202@gmail.com>
2023-08-30 22:10:43 +0300

    refactor compare

--- src/compare.js
+++ src/compare.js
@@ -2,26 +2,25 @@ import _ from 'lodash';
 
 export default (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const result = keys
+  const diff = keys
     .reduce((acc, key) => {
+      const prop1 = `${key}: ${obj1[key]}`;
+      const prop2 = `${key}: ${obj2[key]}`;
+
       if (_.has(obj1, key) && _.has(obj2, key)) {
-        const prop1 = `${key}: ${obj1[key]}`;
-        const prop2 = `${key}: ${obj2[key]}`;
-        return obj1[key] === obj2[key] 
+        return obj1[key] === obj2[key]
           ? [...acc, [' ', prop1]]
           : [...acc, ['-', prop1], ['+', prop2]];
       }
 
       if (_.has(obj1, key) && !_.has(obj2, key)) {
-        const prop = `${key}: ${obj1[key]}`;
-        return [...acc, ['-', prop]];
+        return [...acc, ['-', prop1]];
       }
-      const prop = `${key}: ${obj2[key]}`;
-      return [...acc, ['+', prop]];
+      return [...acc, ['+', prop2]];
     }, [])
-    .sort((a, b) => (a[1][0] === b[1][0] ? 1 : a[1].localeCompare(b[1])))
-    .map((line) => `  ${line[0]} ${line[1]}`)
+    .sort(([, prop1], [, prop2]) => (prop1[0] === prop2[0] ? 1 : prop1.localeCompare(prop2)))
+    .map(([sign, prop]) => `  ${sign} ${prop}`)
     .join('\n');
 
-  return `{\n${result}\n}`;
+  return `{\n${diff}\n}`;
 };
