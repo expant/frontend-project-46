922f4f16b1ee4fab0115a3cbb4b5db8339ba1749
Expant <anton532202@gmail.com>
2023-08-31 11:02:00 +0300

    refactor app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import { readFileSync } from 'node:fs';
-// import path from 'node:path';
+import { resolve } from 'node:path';
 import { cwd } from 'node:process';
 
-import compare from './compare.js';
+import genDiff from './genDiff.js';
 
 export default (filepath1, filepath2) => {
-  const path1 = `${cwd()}/src/files/${filepath1}`;
-  const path2 = `${cwd()}/src/files/${filepath2}`;
+  const path1 = resolve(cwd(), filepath1);
+  const path2 = resolve(cwd(), filepath2);
 
   const file1 = readFileSync(path1, { encoding: 'utf-8' });
   const file2 = readFileSync(path2, { encoding: 'utf-8' });
@@ -14,5 +14,5 @@ export default (filepath1, filepath2) => {
   const obj1 = JSON.parse(file1);
   const obj2 = JSON.parse(file2);
 
-  return compare(obj1, obj2);
+  return genDiff(obj1, obj2);
 };
