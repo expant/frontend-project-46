04923a56e498dafc7508a4629e394d30ff41a5d7
Expant <anton532202@gmail.com>
2023-08-31 16:50:22 +0300

    update test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -4,26 +4,35 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/genDiff.js';
 
 test('gendiff', () => {
-  const obj1 = {
+  const obj1 = { name: 'Anton', age: 23 };
+  const obj2 = { sex: 'male' };
+  const correct1 = `{
+  - age: 23
+  - name: Anton
+  + sex: male
+}`
+
+  const obj3 = {
     host: 'hexlet.io',
     timeout: 50,
     proxy: '123.234.53.22',
     follow: false,
   };
-  const obj2 = {
+  const obj4 = {
     timeout: 20,
     verbose: true,
     host: 'hexlet.io',
   };
 
-  const correctResult = `{
-    - follow: false
-      host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
-  }`;
+  const correct2 = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
 
-  expect(genDiff(obj1, obj2)).toEqual(correctResult);
+  expect(genDiff(obj1, obj2)).toBe(correct1);
+  expect(genDiff(obj3, obj4)).toBe(correct2);
 });
