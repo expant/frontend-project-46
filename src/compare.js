a095b671a03daf9ca20d2e375d93a4a18f722366
Expant <anton532202@gmail.com>
2023-08-31 10:03:37 +0300

    refactor compare

--- src/compare.js
+++ src/compare.js
@@ -1,30 +1,33 @@
 import _ from 'lodash';
 
+const objToStrBySign = (obj) => {
+  if (_.has(obj, 'sign')) {
+    return `  ${obj.sign} ${obj.key}: ${obj.val}`;
+  }
+  return `    ${obj.key}: ${obj.val}`;
+};
+
 export default (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const diff = keys
-    .reduce((acc, key) => {
-      const prop1 = `${key}: ${obj1[key]}`;
-      const prop2 = `${key}: ${obj2[key]}`;
+  const diff = keys.reduce((acc, key) => {
+    const prop1 = { key, val: obj1[key] };
+    const prop2 = { key, val: obj2[key] };
+    const propWithMinusSign = { sign: '-', ...prop1 };
+    const propWithPlusSign = { sign: '+', ...prop2 };
 
-      if (_.has(obj1, key) && _.has(obj2, key)) {
-        return obj1[key] === obj2[key]
-          ? [...acc, [' ', prop1]]
-          : [...acc, ['-', prop1], ['+', prop2]];
-      }
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      return obj1[key] === obj2[key]
+        ? [...acc, prop1]
+        : [...acc, propWithMinusSign, propWithPlusSign];
+    }
 
-      if (_.has(obj1, key) && !_.has(obj2, key)) {
-        return [...acc, ['-', prop1]];
-      }
-      return [...acc, ['+', prop2]];
-    }, [])
-    .sort(([, prop1], [, prop2]) => { 
-      const firstLetterOfKey1 = prop1[0];
-      const firstLetterOfKey2 = prop2[0];
-      return firstLetterOfKey1 === firstLetterOfKey2 ? 1 : prop1.localeCompare(prop2);
-    })
-    .map(([sign, prop]) => `  ${sign} ${prop}`)
-    .join('\n');
+    if (_.has(obj1, key) && !_.has(obj2, key)) {
+      return [...acc, propWithMinusSign];
+    }
+    return [...acc, propWithPlusSign];
+  }, []);
 
-  return `{\n${diff}\n}`;
+  const sorted = _.sortBy(diff, 'key');
+  const result = sorted.map(objToStrBySign).join('\n');
+  return `{\n${result}\n}`;
 };
