22d6aa889a2970f9ed78d893143f0fa099cc2b4c
Expant <anton532202@gmail.com>
2023-09-05 16:53:28 +0300

    start update parses module

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import { readFileSync } from 'node:fs';
 import { cwd } from 'node:process';
 import path from 'node:path';
+import yaml from 'js-yaml';
 
 import gendiff from './genDiff.js';
 
 export default (filepath1, filepath2) => {
   const path1 = path.resolve(cwd(), filepath1);
   const path2 = path.resolve(cwd(), filepath2);
+
 
   const file1 = readFileSync(path1, 'utf-8');
   const file2 = readFileSync(path2, 'utf-8');
---
+++ src/parsers.js
@@ -0,0 +1,14 @@
+import { readFileSync } from 'node:fs';
+import { cwd } from 'node:process';
+import path from 'node:path';
+import yaml from 'js-yaml';
+
+import gendiff from './genDiff.js';
+
+const yamlToObj = () => ;
+
+const jsonToObj = () => ;
+
+export default (filepath1, filepath2) => {
+  
+};
