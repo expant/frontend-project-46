71692a03d506d516113baf687ee79583272be72e
Expant <anton532202@gmail.com>
2023-09-05 13:54:56 +0300

    update

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,13 +1,22 @@
 // @ts-check
 
 import { test, expect } from '@jest/globals';
-import genDiff from '../src/genDiff.js';
+import path from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import gendiff from '../src/gendiff.js';
 
-import file1 from '../__fixtures__/file1.json';
-import file2 from '../__fixtures__/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('empty objects', () => {
+  expect(gendiff({}, {})).toBe('{}');
+});
 
 test('gendiff', () => {
-  const expected = `{
+  const expected1 = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -16,5 +25,18 @@ test('gendiff', () => {
   + verbose: true
 }`;
 
-  expect(genDiff(file1, file2)).toBe(expected);
+  const expected2 = `{
+  - follow: false
+  - host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+}`;
+
+  const file1 = readFileSync(getFixturePath('file1.json'), 'utf-8');
+  const file2 = readFileSync(getFixturePath('file2.json'), 'utf-8');
+  const obj1 = JSON.parse(file1);
+  const obj2 = JSON.parse(file2);
+
+  expect(gendiff(obj1, obj2)).toBe(expected1);
+  expect(gendiff(obj1, {})).toBe(expected2);
 });
