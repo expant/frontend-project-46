30ab0aa9ac222bde06f4766c4b6652bf0ce5ad3d
Expant <anton532202@gmail.com>
2023-10-09 16:39:39 +0300

    add formatter test with .yml, .yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -35,11 +35,23 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 //   expect(parse(yamlFile, yamlFormat)).toEqual(expectedYamlFile);
 // });
 
-test('gendiff', () => {
+test('gendiff /.json', () => {
   const file1 = readFileSync(getFixturePath('file1.json'), 'utf-8');
   const file2 = readFileSync(getFixturePath('file2.json'), 'utf-8');
   const obj1 = JSON.parse(file1);
   const obj2 = JSON.parse(file2);
+
+  const expectedFile1 = readFileSync(getFixturePath('expectedFile1.txt'), 'utf-8');
+  const diff = genDiff(obj1, obj2);
+  expect(genStylishFormat(diff)).toEqual(expectedFile1); 
+});
+
+
+test('gendiff /.yml /.yaml', () => {
+  const file1 = readFileSync(getFixturePath('file1.yml'), 'utf-8');
+  const file2 = readFileSync(getFixturePath('file2.yaml'), 'utf-8');
+  const obj1 = yaml.load(file1);
+  const obj2 = yaml.load(file2);
 
   const expectedFile1 = readFileSync(getFixturePath('expectedFile1.txt'), 'utf-8');
   const diff = genDiff(obj1, obj2);
