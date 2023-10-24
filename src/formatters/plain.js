007279c9a0c053cf576a4aa314dafd4cf74b8b17
Expant <anton532202@gmail.com>
2023-10-24 16:31:10 +0300

    update

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,5 +1,27 @@
 import _ from 'lodash';
 
-export default (diff) => {
-  
+const plain = (diff) => {
+  console.log(diff);
+  const list = diff.map((node) => {
+    const { val } = node;
+
+    if (!Array.isArray(val)) {
+      const { sign, key } = node;
+
+      if (sign === '+') {
+        return `Property '${key}' was added with value: ${val}`;
+      } else if (sign === '-') {
+        return `Property '${key}' was removed`;
+      } else if (sign === '') {
+        return '';
+      }
+    }
+
+    return plain(val);
+ 
+  });
+
+  return list.filter((line) => line !== '').join('\n');
 };
+
+export default plain;
--- src/genDiff.js
+++ src/genDiff.js
@@ -6,29 +6,29 @@ const genDiff = (obj1, obj2) => {
   const diff = keys.flatMap((key) => {
     if (_.has(obj1, key) && _.has(obj2, key)) {
       if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-        return { 
-          key, 
-          val: genDiff(obj1[key], obj2[key]) 
-        };
+        return { sign: '', key, val: genDiff(obj1[key], obj2[key]) };
       }
 
       if (obj1[key] === obj2[key]) {
-        const val = obj1[key];
-        return { key, val };
+        return { sign: '', key, val: obj1[key] };
       }
-      const prevVal = obj1[key];
-      const newVal = obj2[key];
-      return { state: 'updated', key, prevVal, newVal };
+      return { sign: '', key, prevVal: obj1[key], newVal: obj2[key] };
+      // const prop1 = { sign: '-', key, val: obj1[key] };
+      // const prop2 = { sign: '+', key, val: obj2[key] };
+      // return obj1[key] === obj2[key] ? { ...prop1, sign: '' } : [prop1, prop2];
     }
 
+    const isComplex = true; 
+    const complexObj1 = { sign: '-', key, val: _.cloneDeep(obj1[key]), isComplex };
+    const complexObj2 = { sign: '+', key, val: _.cloneDeep(obj2[key]), isComplex };
     if (_.has(obj1, key) && !_.has(obj2, key)) {
-      return { state: 'removed', key, val: _.cloneDeep(obj1[key]) };
+      return complexObj1;
     }
-    return { state: 'added', key, val: _.cloneDeep(obj2[key]) };
+    return complexObj2;
   });
-
   return _.sortBy(diff, 'key');
 };
+
 
 export default genDiff;
 
