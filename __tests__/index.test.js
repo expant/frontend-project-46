58da51f00c4e0f5e54061f2a9f7289594f801d56
Expant <anton532202@gmail.com>
2023-10-27 14:00:53 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,33 +9,34 @@ import { fileURLToPath } from 'url';
 import genDiff from '../src/genDiff.js';
 import genStylishFormat from '../src/formatters/stylish.js';
 import genPlainFormat from '../src/formatters/plain.js';
+import genJSONFormat from '../src/formatters/json.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('gendiff /.json', () => {
-  const file1 = readFileSync(getFixturePath('file1.json'), 'utf-8');
-  const file2 = readFileSync(getFixturePath('file2.json'), 'utf-8');
-  const obj1 = JSON.parse(file1);
-  const obj2 = JSON.parse(file2);
+test('test /.json /.yml /.yaml', () => {
+  const jsonFile1 = readFileSync(getFixturePath('file1.json'), 'utf-8');
+  const jsonFile2 = readFileSync(getFixturePath('file2.json'), 'utf-8');
+  const obj1 = JSON.parse(jsonFile1);
+  const obj2 = JSON.parse(jsonFile2);
+
+  const ymlFile = readFileSync(getFixturePath('file1.yml'), 'utf-8');
+  const yamlFile = readFileSync(getFixturePath('file2.yaml'), 'utf-8');
+  const obj3 = yaml.load(ymlFile);
+  const obj4 = yaml.load(yamlFile);
 
   const expectedStylishFormat = readFileSync(getFixturePath('expectedStylishFormat.txt'), 'utf-8');
   const expectedPlainFormat = readFileSync(getFixturePath('expectedPlainFormat.txt'), 'utf-8');
-  const diff = genDiff(obj1, obj2);
-  expect(genStylishFormat(diff)).toEqual(expectedStylishFormat);
-  expect(genPlainFormat(diff)).toEqual(expectedPlainFormat);
-});
+  const expectedJSONFormat = readFileSync(getFixturePath('expectedJSONFormat.txt'), 'utf-8');
 
-test('gendiff /.yml /.yaml', () => {
-  const file1 = readFileSync(getFixturePath('file1.yml'), 'utf-8');
-  const file2 = readFileSync(getFixturePath('file2.yaml'), 'utf-8');
-  const obj1 = yaml.load(file1);
-  const obj2 = yaml.load(file2);
+  const jsonDiff = genDiff(obj1, obj2);
+  const ymlDiff = genDiff(obj3, obj4);
 
-  const expectedStylishFormat = readFileSync(getFixturePath('expectedStylishFormat.txt'), 'utf-8');
-  const expectedPlainFormat = readFileSync(getFixturePath('expectedPlainFormat.txt'), 'utf-8');
-  const diff = genDiff(obj1, obj2);
-  expect(genStylishFormat(diff)).toEqual(expectedStylishFormat);
-  expect(genPlainFormat(diff)).toEqual(expectedPlainFormat);
+  expect(genStylishFormat(jsonDiff)).toEqual(expectedStylishFormat);
+  expect(genPlainFormat(jsonDiff)).toEqual(expectedPlainFormat);
+  expect(genJSONFormat(jsonDiff)).toEqual(expectedJSONFormat);
+  expect(genStylishFormat(ymlDiff)).toEqual(expectedStylishFormat);
+  expect(genPlainFormat(ymlDiff)).toEqual(expectedPlainFormat);
+  expect(genJSONFormat(ymlDiff)).toEqual(expectedJSONFormat);
 });
