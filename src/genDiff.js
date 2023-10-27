9f91b8fef7126bdb8c5a0418d54b3b5e2f05905e
Expant <anton532202@gmail.com>
2023-10-27 11:23:07 +0300

    refactor genDiff

--- src/genDiff.js
+++ src/genDiff.js
@@ -11,34 +11,46 @@ const genComplex = (node) => {
     });
 };
 
+const checkWithTheSameKeys = (obj1, obj2, key) => {
+  const valObj1 = obj1[key];
+  const valObj2 = obj2[key];
+
+  if (_.isObject(valObj1) && _.isObject(valObj2)) {
+    return {
+      state: 'noChanged',
+      key,
+      val: genDiff(valObj1, valObj2),
+    };
+  }
+
+  if (valObj1 === valObj2) {
+    return { state: 'noChanged', key, val: valObj1 };
+  }
+
+  const val = genComplex(valObj1);
+  const newVal = genComplex(valObj2);
+  return {
+    state: 'updated', key, val, newVal,
+  };
+};
+
+const checkWithTheNotSameKeys = (obj1, obj2, key) => {
+  if (_.has(obj1, key) && !_.has(obj2, key)) {
+    const val = _.isObject(obj1[key]) ? genComplex(obj1[key]) : obj1[key];
+    return { state: 'removed', key, val };
+  }
+  const val = _.isObject(obj2[key]) ? genComplex(obj2[key]) : obj2[key];
+  return { state: 'added', key, val };
+};
+
 const genDiff = (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
   const diff = keys.flatMap((key) => {
     if (_.has(obj1, key) && _.has(obj2, key)) {
-      if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-        return {
-          state: 'noChanged',
-          key,
-          val: genDiff(obj1[key], obj2[key]),
-        };
-      }
-
-      if (obj1[key] === obj2[key]) {
-        return { state: 'noChanged', key, val: obj1[key] };
-      }
-      const val = genComplex(obj1[key]);
-      const newVal = genComplex(obj2[key]);
-      return {
-        state: 'updated', key, val, newVal,
-      };
+      return checkWithTheSameKeys(obj1, obj2, key);
     }
 
-    if (_.has(obj1, key) && !_.has(obj2, key)) {
-      const val = _.isObject(obj1[key]) ? genComplex(obj1[key]) : obj1[key];
-      return { state: 'removed', key, val };
-    }
-    const val = _.isObject(obj2[key]) ? genComplex(obj2[key]) : obj2[key];
-    return { state: 'added', key, val };
+    return checkWithTheNotSameKeys(obj1, obj2, key);
   });
   return _.sortBy(diff, 'key');
 };
