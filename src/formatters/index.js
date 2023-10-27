7be103d28a9d859ced46692008fdd505bb166ba1
Expant <anton532202@gmail.com>
2023-10-27 14:00:07 +0300

    realize json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,15 @@
 import genDiff from '../genDiff.js';
 import plain from './plain.js';
 import stylish from './stylish.js';
+import json from './json.js';
 
 export default (obj1, obj2, formatName) => {
   const diff = genDiff(obj1, obj2);
-  if (formatName === 'plain') {
-    return plain(diff);
+  switch (formatName) {
+    case 'plain': return plain(diff);
+    case 'stylish': return stylish(diff);
+    case 'json': return json(diff);
+    default: 
+      throw new Error(`Unknown formatName: ${formatName}`);
   }
-  return stylish(diff);
 };
