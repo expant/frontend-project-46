c14b672e2220f194c04034710da6009db18f7648
Expant <anton532202@gmail.com>
2023-10-27 10:02:08 +0300

    refactor stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,41 +2,49 @@ const INDENTS_COUNT = 4;
 const LEFT_SHIFT = 2;
 const REPLACER = ' ';
 
+const getIndents = (size) => {
+  const indent = REPLACER.repeat(size);
+  const indentWithSign = REPLACER.repeat(size - LEFT_SHIFT);
+  const bracketIndent = REPLACER.repeat(size - INDENTS_COUNT);
+  return [indent, indentWithSign, bracketIndent];
+};
+
+const buildLine = (node, indents) => {
+  const { state, key, val } = node;
+  const [indent, indentWithSign] = indents;
+  let line;
+
+  switch (state) {
+    case 'updated': {
+      const line1 = `${indentWithSign}- ${key}: ${val}`;
+      const line2 = `${indentWithSign}+ ${key}: ${node.newVal}`;
+      line = [line1, line2].join('\n');
+      break;
+    }
+    case 'added':
+      line = `${indentWithSign}+ ${key}: ${val}`;
+      break;
+    case 'removed':
+      line = `${indentWithSign}- ${key}: ${val}`;
+      break;
+    case 'noChanged':
+      line = `${indent}${key}: ${val}`;
+      break;
+    default: throw new Error(`Unknown state: ${state}`);
+  }
+  return line;
+};
+
 export default (diff) => {
   const iter = (node, depth) => {
-    if (!Array.isArray(node)) {
-      return `${node}`;
-    }
+    if (!Array.isArray(node)) return `${node}`;
 
     const indentSize = depth * INDENTS_COUNT;
-    const indent = REPLACER.repeat(indentSize);
-    const indentWithSign = REPLACER.repeat(indentSize - LEFT_SHIFT);
-    const bracketIndent = REPLACER.repeat(indentSize - INDENTS_COUNT);
-
-    const lines = node.map((line) => {
-      const { state, key } = line;
-      const val = iter(line.val, depth + 1);
-      let result;
-
-      switch (state) {
-        case 'updated': {
-          const line1 = `${indentWithSign}- ${key}: ${val}`;
-          const line2 = `${indentWithSign}+ ${key}: ${line.newVal}`;
-          result = [line1, line2].join('\n');
-          break;
-        }
-        case 'added':
-          result = `${indentWithSign}+ ${key}: ${val}`;
-          break;
-        case 'removed':
-          result = `${indentWithSign}- ${key}: ${val}`;
-          break;
-        case 'noChanged':
-          result = `${indent}${key}: ${val}`;
-          break;
-        default: throw new Error(`Unknown state: ${state}`);
-      }
-      return result;
+    const [indent, indentWithSign, bracketIndent] = getIndents(indentSize);
+    const lines = node.map((obj, indents) => {
+      const val = iter(obj.val, depth + 1);
+      const line = buildLine({ ...obj, val }, [indent, indentWithSign]); 
+      return line;
     });
 
     return [
