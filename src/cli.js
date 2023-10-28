c8043234794c37ec212751de765875a051617ba2
Expant <anton532202@gmail.com>
2023-10-29 01:28:18 +0300

    refactor cli

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,19 @@
 import { program } from 'commander';
 import genDiff from '../index.js';
 
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    const { format } = program.opts();
-    const diff = genDiff(filepath1, filepath2, format);
-    console.log(diff);
-  });
+export default () => {
+  program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format', 'stylish')
+    .action((filepath1, filepath2) => {
+      const { format } = program.opts();
+      const diff = genDiff(filepath1, filepath2, format);
+      console.log(diff);
+    });
 
-program.parse();
+  program.parse();
+};
