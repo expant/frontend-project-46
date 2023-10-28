81e423a91ef8f175d232d6309a624cf1fa2421b7
Expant <anton532202@gmail.com>
2023-10-29 01:19:57 +0300

    refactor switch default

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -5,9 +5,7 @@ import json from './json.js';
 export default (formatName) => {
   switch (formatName) {
     case 'plain': return plain;
-    case 'stylish': return stylish;
     case 'json': return json;
-    default:
-      throw new Error(`Unknown formatName: ${formatName}`);
+    default: return stylish;
   }
 };
