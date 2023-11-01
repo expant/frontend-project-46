e18cf23788de67a2923483984471611658f6f264
Expant <anton532202@gmail.com>
2023-11-01 16:48:37 +0300

    refactor the top func index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import { readFileSync } from 'node:fs';
 import { cwd } from 'node:process';
 import { resolve, extname } from 'node:path';
-
 import genDiff from './genDiff.js';
 import chooseFormat from './formatters/index.js';
 import parse from './parsers.js';
 
-const getFile = (filepath) => {
+const extractFormat = (file, filepath) => {
+  const extnameWithDot = extname(filepath);
+  return extnameWithDot.slice(1, extnameWithDot.length);  
+}
+
+const readFile = (filepath) => {
   const path = resolve(cwd(), '__fixtures__', filepath);
   const file = readFileSync(path, 'utf-8');
-  return file;
+  const format = extractFormat(file, filepath);
+  return parse(file, format);
 };
 
 export default (filepath1, filepath2, formatName) => {
-  const file1 = getFile(filepath1);
-  const file2 = getFile(filepath2);
-
-  const obj1 = parse(file1, extname(filepath1));
-  const obj2 = parse(file2, extname(filepath2));
   const format = chooseFormat(formatName);
-  return format(genDiff(obj1, obj2));
+  return format(
+    genDiff(readFile(filepath1), readFile(filepath2))
+  );
 };
