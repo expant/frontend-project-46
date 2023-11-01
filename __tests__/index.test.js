480ebf4825622b94f2ed4e1e99cc23e324f6a4b9
Expant <anton532202@gmail.com>
2023-11-01 16:49:07 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 // @ts-nocheck
 
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import path, { dirname } from 'path';
 import yaml from 'js-yaml';
 import { readFileSync } from 'fs';
@@ -15,28 +15,39 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('/.json /.yml /.yaml', () => {
-  const jsonFile1 = readFileSync(getFixturePath('file1.json'), 'utf-8');
-  const jsonFile2 = readFileSync(getFixturePath('file2.json'), 'utf-8');
-  const obj1 = JSON.parse(jsonFile1);
-  const obj2 = JSON.parse(jsonFile2);
+test.each(['json', 'yml', 'yaml'])('gendiff (%s) format', () => {
+  const filepath1 = readFileSync(getFixturePath('file1.%s'), 'utf-8');
+  const filepath2 = readFileSync(getFixturePath('file2.%s'), 'utf-8');
+  
+  const data1 = '%s' === 'json' ? JSON.parse(filepath1) : yaml.load(filepath1);
+  const data2 = '%s' === 'json' ? JSON.parse(filepath2) : yaml.load(filepath2);
 
-  const ymlFile = readFileSync(getFixturePath('file1.yml'), 'utf-8');
-  const yamlFile = readFileSync(getFixturePath('file2.yaml'), 'utf-8');
-  const obj3 = yaml.load(ymlFile);
-  const obj4 = yaml.load(yamlFile);
+  expect(genDiff(data1, data2)).toBe()
 
-  const expectedStylishFormat = readFileSync(getFixturePath('expectedStylishFormat.txt'), 'utf-8');
-  const expectedPlainFormat = readFileSync(getFixturePath('expectedPlainFormat.txt'), 'utf-8');
-  const expectedJSONFormat = readFileSync(getFixturePath('expectedJSONFormat.txt'), 'utf-8');
-
-  const jsonDiff = genDiff(obj1, obj2);
-  const ymlDiff = genDiff(obj3, obj4);
-
-  expect(genStylishFormat(jsonDiff)).toEqual(expectedStylishFormat);
-  expect(genPlainFormat(jsonDiff)).toEqual(expectedPlainFormat);
-  expect(genJSONFormat(jsonDiff)).toEqual(expectedJSONFormat);
-  expect(genStylishFormat(ymlDiff)).toEqual(expectedStylishFormat);
-  expect(genPlainFormat(ymlDiff)).toEqual(expectedPlainFormat);
-  expect(genJSONFormat(ymlDiff)).toEqual(expectedJSONFormat);
 });
+
+// test('/.json /.yml /.yaml', () => {
+//   const jsonFile1 = readFileSync(getFixturePath('file1.json'), 'utf-8');
+//   const jsonFile2 = readFileSync(getFixturePath('file2.json'), 'utf-8');
+//   const obj1 = JSON.parse(jsonFile1);
+//   const obj2 = JSON.parse(jsonFile2);
+
+//   const ymlFile = readFileSync(getFixturePath('file1.yml'), 'utf-8');
+//   const yamlFile = readFileSync(getFixturePath('file2.yaml'), 'utf-8');
+//   const obj3 = yaml.load(ymlFile);
+//   const obj4 = yaml.load(yamlFile);
+
+//   const expectedStylishFormat = readFileSync(getFixturePath('expectedStylishFormat.txt'), 'utf-8');
+//   const expectedPlainFormat = readFileSync(getFixturePath('expectedPlainFormat.txt'), 'utf-8');
+//   const expectedJSONFormat = readFileSync(getFixturePath('expectedJSONFormat.txt'), 'utf-8');
+
+//   const jsonDiff = genDiff(obj1, obj2);
+//   const ymlDiff = genDiff(obj3, obj4);
+
+//   expect(genStylishFormat(jsonDiff)).toEqual(expectedStylishFormat);
+//   expect(genPlainFormat(jsonDiff)).toEqual(expectedPlainFormat);
+//   expect(genJSONFormat(jsonDiff)).toEqual(expectedJSONFormat);
+//   expect(genStylishFormat(ymlDiff)).toEqual(expectedStylishFormat);
+//   expect(genPlainFormat(ymlDiff)).toEqual(expectedPlainFormat);
+//   expect(genJSONFormat(ymlDiff)).toEqual(expectedJSONFormat);
+// });
