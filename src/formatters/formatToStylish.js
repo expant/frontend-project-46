e7258b5b8ccc0d0a53eedf10ac0585eb7e9609cb
Expant <anton532202@gmail.com>
2023-11-28 22:39:46 +0300

    refactor

--- src/formatters/formatToStylish.js
+++ src/formatters/formatToStylish.js
@@ -1,17 +1,34 @@
+import _, { entries } from 'lodash';
+
 const INDENTS_COUNT = 4;
 const LEFT_SHIFT = 2;
 const REPLACER = ' ';
 
 const getIndents = (size) => {
-  const indent = REPLACER.repeat(size);
-  const indentWithSign = REPLACER.repeat(size - LEFT_SHIFT);
+  // const indent = REPLACER.repeat(size);
+  const indent = REPLACER.repeat(size - LEFT_SHIFT);
   const bracketIndent = REPLACER.repeat(size - INDENTS_COUNT);
-  return [indent, indentWithSign, bracketIndent];
+  return [indent, bracketIndent];
 };
 
-// const stringify = (data) => {
-  
-// };
+const signs = {
+  added: '+',
+  removed: '-',
+  noChanged: ' ',
+};
+
+const stringify = (data, type) => {
+  if (!_.isObject(data)) {
+    return `${data}`;
+  }
+
+  const [indent, bracketIndent] = getIndents()
+  return Object
+    .entries(data)
+    .map(([key, val]) => {
+      return `${indent}${signs[type]} ${key}: ${stringify(val)}`
+    });
+};
 
 export default (diff) => {
   const iter = (node, depth) => {
@@ -22,20 +39,20 @@ export default (diff) => {
     const lines = node.map((obj) => {
       const { type, key } = obj;
       switch (type) {
-        case 'added': return `${indentWithSign}+ ${key}: ${iter(obj.val, depth + 1)}`;
-        case 'removed': return `${indentWithSign}- ${key}: ${iter(obj.val, depth + 1)}`;
+        case 'added': return `${indentWithSign}${signs.added} ${key}: ${stringify(obj.val)}`; // stringify()
+        case 'removed': return `${indentWithSign}${signs.removed} ${key}: ${stringify(obj.val)}`; // stringify()
         case 'updated': {
-          const val1 = iter(obj.val1, depth + 1);
-          const val2 = iter(obj.val2, depth + 1);
-          const line1 = `${indentWithSign}- ${key}: ${val1}`;
-          const line2 = `${indentWithSign}+ ${key}: ${val2}`;
+          const val1 = stringify(obj.val1);
+          const val2 = stringify(obj.val2);
+          const line1 = `${indentWithSign}- ${key}: ${val1}`; // stringify()
+          const line2 = `${indentWithSign}+ ${key}: ${val2}`; // stringify()
           return [line1, line2].join('\n');
         }
         case 'nested': {
           const children = iter(obj.children, depth + 1);
-          return `${indent}${key}: ${children}`;
+          return `${indent}${key}: ${children}`; 
         }
-        case 'noChanged': return `${indent}${key}: ${iter(obj.val, depth + 1)}`;
+        case 'noChanged': return `${indent}${key}: ${stringify(obj.val)}`;
         default: throw new Error(`Unknown type: ${type}`);   
       }
     }); 
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,28 +1,28 @@
 import _ from 'lodash';
 
-const getValOnType = (node) => {
-  if (!_.isObject(node)) return node;
-  return Object
-    .entries(node)
-    .flatMap(([key, val]) => {
-      if (!_.isObject(val)) return { type: 'noChanged', key, val };
-      return { type: 'nested', key, children: getValOnType(val) };
-    });
-};
+// const getValOnType = (node) => {
+//   if (!_.isObject(node)) return node;
+//   return Object
+//     .entries(node)
+//     .flatMap(([key, val]) => {
+//       if (!_.isObject(val)) return { type: 'noChanged', key, val };
+//       return { type: 'nested', key, children: getValOnType(val) };
+//     });
+// };
 
-const checkWithEqualKeys = (data1, data2, key) => {
-  const dataVal1 = data1[key];
-  const dataVal2 = data2[key];
+// const checkWithEqualKeys = (data1, data2, key) => {
+//   const dataVal1 = data1[key];
+//   const dataVal2 = data2[key];
 
-  if (dataVal1 !== dataVal2) {
-    const val1 = getValOnType(dataVal1);
-    const val2 = getValOnType(dataVal2);
-    return {
-      type: 'updated', key, val1, val2,
-    };
-  }
-  return { type: 'noChanged', key, val: dataVal1 };
-};
+//   if (dataVal1 !== dataVal2) {
+//     const val1 = getValOnType(dataVal1);
+//     const val2 = getValOnType(dataVal2);
+//     return {
+//       type: 'updated', key, val1, val2,
+//     };
+//   }
+//   return { type: 'noChanged', key, val: dataVal1 };
+// };
 
 const genDiff = (data1, data2) => {
   const keys1 = Object.keys(data1);
@@ -31,17 +31,26 @@ const genDiff = (data1, data2) => {
 
   return keys.map((key) => {
     if (!_.has(data1, key)) {
-      return { type: 'added', key, val: getValOnType(data2[key]) };
+      // return { type: 'added', key, val: getValOnType(data2[key]) };
+      return { type: 'added', key, val: data2[key] };
     }
     if (!_.has(data2, key)) {
-      return { type: 'removed', key, val: getValOnType(data1[key]) };
+      // return { type: 'removed', key, val: getValOnType(data1[key]) };
+      return { type: 'removed', key, val: data1[key] };
     }
 
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
       const children = genDiff(data1[key], data2[key]);
       return { type: 'nested', key, children };
     }
-    return checkWithEqualKeys(data1, data2, key);
+    if (data1[key] !== data2[key]) {
+      return {
+        type: 'updated', key, val1: data1[key], val2: data2[key],
+      };
+    }
+    return { type: 'noChanged', key, val: data1[key] };
+
+    // return checkWithEqualKeys(data1, data2, key);
   });
 };
 
